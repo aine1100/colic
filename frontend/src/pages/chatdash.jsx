55d1782dc9d139fcac7d4af03c4f97e444876d76
aine1100 <149615514+aine1100@users.noreply.github.com>
2024-09-27 09:27:16 +0200

    updateone

--- frontend/src/pages/chatdash.jsx
+++ frontend/src/pages/chatdash.jsx
@@ -1,9 +1,28 @@
+import { FaAudioDescription, FaMicrophone, FaVideo } from "react-icons/fa";
 import Sidenav from "../components/sidenav";
+import UserImage from "../images/user.png"
 
 export const Chatdash = () => {
   return (
-    <div className="bg-black w-full h-screen text-white">
+    <div className="bg-black w-full h-screen text-white flex gap-5">
                 <Sidenav/>
+              <div className="flex flex-col gap-10 bg-[#817575] bg-opacity-10 rounded-md  ">
+              <div className="flex justify-center items-start bg-black p-7 ">
+                   <div className="flex gap-12 items-center justify-center">
+                   <img src={UserImage} alt="" className="w-20 h-20 rounded-[50%]" />
+                   <p className="text-[25px]">Dave Robinson</p>
+
+                   </div>
+                   <div className="flex gap-20 items-center mt-5 pl-[600px] justify-center">
+                  <FaVideo className="text-[40px]"/>
+                  <FaMicrophone className="text-[40px]"/>
+                   </div>
+                </div>
+                <div className=" rounded-md ">
+
+                </div>
+
+              </div>
     </div>
   )
 }
